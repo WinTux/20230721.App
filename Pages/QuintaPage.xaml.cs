a92642c60de02aaaf8a9d1ae0adc8122841f03ff
Rubén Gonzalo Soria Soria <rusok.trabajo@hotmail.com>
2023-07-28 20:00:44 -0400

    Pines y polígonos

--- Pages/QuintaPage.xaml.cs
+++ Pages/QuintaPage.xaml.cs
@@ -1,3 +1,7 @@
+using Microsoft.Maui.Maps;
+using Microsoft.Maui.Controls.Maps;
+using System.Diagnostics;
+
 namespace _20230721.Pages;
 
 public partial class QuintaPage : ContentPage
@@ -5,5 +9,19 @@ public partial class QuintaPage : ContentPage
 	public QuintaPage()
 	{
 		InitializeComponent();
+		Location location = new Location(-16.501836579169517, -68.13286819508644);
+		MapSpan mapSpan = new MapSpan(location, 0.01, 0.01);
+		mimapa.MoveToRegion(mapSpan);
+
+		mimapa.Pins.Add(new Pin {
+			Label = "Pepe está en el Banco Unión",
+			Location = new Location(-16.501534637030485, -68.13304757163773)
+		});
+
+		mimapa.MapType = MapType.Satellite;
+	}
+	async void OnMapaClic(Object sender, MapClickedEventArgs e)
+	{
+		Debug.WriteLine($"Map cliqueado en: {e.Location.Latitude},  {e.Location.Longitude}");
 	}
 }
